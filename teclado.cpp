54fd327485c2dee04d238602a533a21b69264bd0
Bruno Marques Maciel <bmarques.maciel@gmail.com>
2017-10-07 15:14:11 -0300

    Find e Store funcionando localmente

--- teclado.cpp
+++ teclado.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
-#define MAX_NOS 32
+#define MAX_ARQUIVOS 10
+#define NOME_ARQ_LISTA_DISPONIVEIS "arquivos.txt"
 #define FIND 0
 #define STORE 1
 #define EXIT -1
@@ -16,13 +18,15 @@ void aguardandoOpcao(int ID);
 void find(int key, int ID);
 int gerarChave();
 int chavePertenceMeuRange(int key, int ID);
-void imprimeArquivo (char* path);
-char* consultaMeusArquivos(int key);
+void imprimeArquivo (string path);
+string consultaMeusArquivos(int key);
 void store(int key, string value, int ID);
 string maiusculo(string str);
 string retiraEspaco(string str);
+int enviaStoreSucessor(int key, string value) ;
+string enviaFindSucessor(int key);
 
-int K;
+int K,num_nos;
 string valor;
 
 //Composto de 'endereço IP' e 'porta'
@@ -36,11 +40,8 @@ HOST sucessor;
 /*****
 sucessor.txt
 
-N K
-IP1 PORT1
-IP2 PORT2
-...
-IPN PORTN
+Minha_Chave IP_Sucessor PORT_Sucessor
+
 *****/
 
 int main(int argc , char *argv[]) {
@@ -171,78 +172,159 @@ int menu() {
 	return ERROR;
 }
 
-int gerarChave() {
+/*int gerarChave() {
   //https://github.com/orcaman/chord/tree/master/src
   return 0;
-}
+}*/
 
 int join(char* nomeArquivo) {
-    int key = gerarChave();
-    //Lê do arquivo os dados do nó sucessor
+		int chave = 0;
+    //Lê do arquivo os dados de chave e do nó sucessor
     FILE *file;
     file = fopen(nomeArquivo, "r");
     if (file) {
         char IP[25]; int PORT;
-        while(fscanf(file,"%s %d", IP, &PORT) != EOF){
-            strcpy(sucessor.IP,IP);
-            sucessor.PORT = PORT;
-        }
+				try {
+				fscanf(file,"%d %d", &chave, &num_nos);
+				fscanf(file,"%s %d", IP, &PORT);
+        strcpy(sucessor.IP,IP);
+        sucessor.PORT = PORT;
+			} catch (...) {
+				cout << "\nErro com a formatação do arquivo\n\n";
+			}
         fclose(file);
     } else {
 			cout << "\n Não foi possível ler o arquivo: "<< nomeArquivo << "!\n\n";
 			exit(1);
 		}
 
-    return key;
+		return chave;
+
 }
+
+/* Considerando que o primeiro valor de chave é 1.
+Assim, ex.:
+N = 3
+MAX_ARQUIVO = 10
+Chaves de 01 a 10 pertencem ao nó com chave 1.
+Chaves de 11 a 20 pertencem ao nó com chave 2.
+Chaves de 21 a 30 pertencem ao nó com chave 3.
+*/
 
 int chavePertenceMeuRange(int key, int ID) {
-  //definido a partir do metodo de geracao de chave escolhido
-  return 1;
+  if (key > MAX_ARQUIVOS*(ID-1) && key <= MAX_ARQUIVOS*ID)
+		return 1;
+	else
+		return 0;
 }
 
-void imprimeArquivo (char* path) {
-	FILE *file;
-  file = fopen(path, "r");
-  if (file) {
-      char* aux;
-      while(fscanf(file, "%s", aux) != EOF){
-          cout << aux;
-      }
-      fclose(file);
-  } else {
-		cout << "\nErro ao abrir arquivo: "<<path;
+void imprimeArquivo (string path) {
+	ifstream FILE;
+	FILE.open(path);
+	if (FILE.is_open()) {
+		string linha;
+		while (!FILE.eof()) {
+			getline(FILE,linha);
+			if (!FILE.eof()) {
+				cout << linha;
+			}
+	  }
+	} else { cout << "\nNenhum arquivo no diretório!\n"; }
+	FILE.close();
+}
+
+//Retorna 0 se já há um arquivo com essa chave, 1 se sucesso
+int armazenaArquivo(int key, string value) {
+	int statusRetorno = 0;
+  ofstream FILE;
+	FILE.open(NOME_ARQ_LISTA_DISPONIVEIS,std::ofstream::app); // cria o arquivo se não existe, se existe adiciona texto no final
+	if (FILE.is_open()) {
+		if (!consultaMeusArquivos(key).compare("ERRO")) { // se já não tem arquivo com essa chave salvo
+			FILE << key << " " << value << endl;
+			statusRetorno = 1;
+		}
+	} else {
+		cout << "\nErro ao abrir arquivo " << NOME_ARQ_LISTA_DISPONIVEIS;
+		exit(1);
 	}
-}
-
-char* consultaMeusArquivos(int key) {
-  // Vai ao arquivo, lê tuplas <key,path>, retorna path da key passada parametro
-	return NULL;
+	FILE.close();
+	return statusRetorno;
 }
 
 void find(int key, int ID) {
 
-    /*if (chavePertenceMeuRange(key,ID)) {
-      char* caminho = consultaMeusArquivos(key); // não compete com requisicao.cpp rodando localmente, pois é leitura
-      if (caminho == NULL) {
-        cout << "\nArquivo não encontrado!";
+	if (key <= 0 || key>(num_nos*MAX_ARQUIVOS)) {
+		cout << "\nChave dos arquivos deve ser maior que 0 e menor que "<< (num_nos*MAX_ARQUIVOS)+1 <<"!\n";
+	}
+	else {
+    if (chavePertenceMeuRange(key,ID)) {
+      string caminho = consultaMeusArquivos(key); // não compete com requisicao.cpp rodando localmente, pois é leitura
+			if (!caminho.compare("ERRO")) {
+        cout << "\nArquivo não encontrado! \n";
       } else {
+				cout << "\nArquivo encontrado - Conteúdo: \n";
         imprimeArquivo(caminho);
+				cout << "\n";
       }
     } else {
-      if (sucessor.find(key) == 0) cout << "\nArquivo não encontrado!";
-    }*/
+			string caminhoLocal = enviaFindSucessor(key);
+			if (caminhoLocal.compare("ERRO")) cout << "\nArquivo não encontrado!\n";
+			else imprimeArquivo(caminhoLocal);
+    }
 
+	}
+
+}
+
+string enviaFindSucessor(int key) {
+	  //recupera o arquivo, cria um arquivo local que é cópia dele, e retorna caminho desse arquivo; se nao achar, retorna ERRO
+		return "ERRO";
+}
+
+int enviaStoreSucessor(int key, string value) {
+	//Envia o arquivo efetivamente
+	return 0;
+}
+
+//retorna NULL se não encontrar
+string consultaMeusArquivos(int key) {
+	ifstream FILE;
+	FILE.open(NOME_ARQ_LISTA_DISPONIVEIS);
+  int c;
+	if (FILE.is_open()) {
+		string linha;
+		while (!FILE.eof()) {
+			getline(FILE,linha);
+			if (!FILE.eof()) {
+				c = stoi(linha.substr(0, linha.find(" ")));
+				if (c == key) return linha.substr(linha.find(" ") + 1);
+			}
+	  }
+	} else {
+		cout << "\nNenhum arquivo disponível!\n\n";
+		exit(0);
+	}
+
+	FILE.close();
+	return "ERRO";
 }
 
 void store(int key, string value, int ID) {
 
-  /*if (chavePertenceMeuRange(key,ID)) {
-    if (consultaMeusArquivos(key) == NULL) {
-        //adiciona linha final arquivo com tupla <key,value> - compete com requisicao.cpp ?
-    } else cout << "\nJá existe arquivo com essa chave!";
-  } else {
-        if (sucessor.store(key,value) == 0) cout << "\nJá existe arquivo com essa chave!";
-  }*/
+	if (key <= 0 || key>(num_nos*MAX_ARQUIVOS)) {
+		cout << "\nChave dos arquivos deve ser maior que 0 e menor que "<< (num_nos*MAX_ARQUIVOS)+1 <<"!\n";
+	}
+	else {
+	  if (chavePertenceMeuRange(key,ID)) {
 
+	    if (armazenaArquivo(key,value) == 0) {
+	        cout << "\nJá existe arquivo com essa chave!\n";
+	    } else {
+				cout << "\nArquivo disponibilizado com sucesso!\n";
+			}
+	  } else {
+	        if (enviaStoreSucessor(key,value) == 0) cout << "\nJá existe arquivo com essa chave!\n";
+					else cout << "\nArquivo disponibilizado com sucesso!\n";
+	  }
+  }
 }
